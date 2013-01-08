dc5068306e30ddf686fe2286e7ddd661ab2a2dda
happyzeyu <happyzeyu@7a394894-9432-4381-9535-169912d1abbc>
2013-01-08 09:01:43 +0000

    - JNyqIDE bug fixed: sub-window can go above the tool bar
    git-svn-id: svn://svn.code.sf.net/p/nyquist/code/trunk/nyquist@80 7a394894-9432-4381-9535-169912d1abbc

---
+++ jnyqide/JNonHiddenableInternalFrame.java
@@ -0,0 +1,49 @@
+package jnyqide;
+
+
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import javax.swing.JInternalFrame;
+
+/**
+ * 
+ * An internal frame that cannot be moved above the DesktopPanel.
+ * 
+ * @author zeyu
+ * @version 1.0 (Jan-3-2013)
+ */
+public class JNonHiddenableInternalFrame extends JInternalFrame {
+
+	JNonHiddenableInternalFrame thisFrame;
+	
+	private void _initialize() {
+		thisFrame = this;
+		thisFrame.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentMoved(ComponentEvent arg0) {
+				if (thisFrame.getLocation().y < 1) {
+					thisFrame.setLocation(thisFrame.getLocation().x, 1);
+				}
+			}
+		});
+	}
+
+	public JNonHiddenableInternalFrame() {
+		super();
+		_initialize();
+	}
+	
+	public JNonHiddenableInternalFrame(String name) {
+		super(name);
+		_initialize();
+	}
+	
+	public JNonHiddenableInternalFrame(String name, boolean someMeans) {
+		super(name, someMeans);
+		_initialize();
+	}
+	
+
+
+}
