14e6013b47cf0c35a502ef2aca7a1f86677d1251
happyzeyu <happyzeyu@7a394894-9432-4381-9535-169912d1abbc>
2013-02-24 03:20:08 +0000

    Browser problem fixed
    git-svn-id: svn://svn.code.sf.net/p/nyquist/code/trunk/nyquist@147 7a394894-9432-4381-9535-169912d1abbc

--- jnyqide/BareBonesBrowserLaunch.java
+++ jnyqide/BareBonesBrowserLaunch.java
@@ -17,6 +17,10 @@ import java.awt.Desktop;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URI;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 
 import javax.swing.JOptionPane;
 
@@ -44,9 +48,13 @@ public class BareBonesBrowserLaunch {
 //                openURL.invoke(null, new Object[] {url});
                 System.out.println("openURL invoked with " + url);
             } else if (osName.startsWith("Windows")) {
-                Runtime.getRuntime().exec(
-                        "rundll32 url.dll, FileProtocolHandler " + 
-			"\"" + url + "\""); // quote url or lose "#" suffixes
+				// The new method -- ZEYU
+				if (loadURL(url) == false) {
+				// use the old ways is unsuccessful
+					url = url.replace('\\','/'); // in browser, \ is not supported. --ZEYU
+					System.out.println(" Win 7 open: " + url);
+					Desktop.getDesktop().browse(java.net.URI.create(url));
+				}
             } else { //assume Unix or Linux
                 String[] browsers = {
                     "htmlview", "firefox", "opera", "konqueror", 
@@ -71,4 +79,87 @@ public class BareBonesBrowserLaunch {
             e.printStackTrace();
         }
     }
+	
+	static class StreamReader extends Thread {
+		private InputStream is;
+		private StringWriter sw = new StringWriter();
+
+		public StreamReader(InputStream is) {
+			this.is = is;
+		}
+
+		public void run() {
+			try {
+				int c;
+				while ((c = is.read()) != -1)
+					sw.write(c);
+			} catch (IOException e) {
+			}
+		}
+
+		public String getResult() {
+			return sw.toString();
+		}
+	}
+
+	/**
+	 * 
+	 * @param location
+	 *            path in the registry
+	 * @param key
+	 *            registry key
+	 * @return registry value or null if not found
+	 */
+	public static final String readRegistry(String location, String key) {
+		try {
+			// Run reg query, then read output with StreamReader (internal
+			// class)
+			Process process;
+			if (key.length() > 0)
+				process = Runtime.getRuntime().exec(
+						"reg query " + '"' + location + "\" /v " + key);
+			else
+				process = Runtime.getRuntime().exec(
+						"reg query " + '"' + location + "\" /ve");
+
+			StreamReader reader = new StreamReader(process.getInputStream());
+			reader.start();
+			process.waitFor();
+			reader.join();
+
+			// Parse out the value
+			String[] parsed = reader.getResult().split("\\s+");
+			if (parsed.length > 1 && key.length() > 0) {
+				return parsed[parsed.length - 1];
+			} else {
+				return reader.getResult().split("REG_SZ")[1].split("\"%1\"")[0].split("--")[0].trim(); 
+				// This is ugly... but it removes some useless options that would produce errors in Win Vista.
+			}
+		} catch (Exception e) {
+		}
+
+		return null;
+	}
+
+	public static boolean loadURL(String url) {
+		// Get name for default browser
+		String valueDefaultBrowser = readRegistry(
+				"HKEY_CURRENT_USER\\SOFTWARE\\MICROSOFT\\WINDOWS\\CUrrentVersion\\Explorer\\FileExts\\.html\\UserChoice",
+				"Progid");
+		System.out.println("Default Browser: " + valueDefaultBrowser);
+
+		// Get Path
+		String regLocation = "HKEY_CLASSES_ROOT\\" + valueDefaultBrowser
+				+ "\\Shell\\open\\command";
+		String command = readRegistry(regLocation, "");
+
+		System.out.println("Command: " + command);
+		try {
+			Runtime.getRuntime().exec(command + " " + url);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 }
