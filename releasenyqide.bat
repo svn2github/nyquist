f8206a5b4c992931f5cfe3014765e00b55c4973d
rbd <rbd@7a394894-9432-4381-9535-169912d1abbc>
2018-09-02 00:56:55 +0000

    Small changes for release on Windows: v3.14
    git-svn-id: svn://svn.code.sf.net/p/nyquist/code/trunk/nyquist@281 7a394894-9432-4381-9535-169912d1abbc

--- releasenyqide.bat
+++ releasenyqide.bat
@@ -1,42 +1,20 @@
 rmdir /s /q nyqrelide
 mkdir nyqrelide
 xcopy runtime nyqrelide\runtime /s /i
-del nyqrelide\runtime\CVS /q
-rmdir nyqrelide\runtime\CVS /q
 copy sys\win\msvc\system.lsp nyqrelide\runtime
 xcopy doc nyqrelide\doc /s /i
-copy sys\win\README.txt nqqrelide\doc\readme-win.txt
-del nyqrelide\doc\CVS /q
-rmdir nyqrelide\doc\CVS /q
+copy sys\win\README.txt nyqrelide\doc\readme-win.txt
 xcopy lib nyqrelide\lib /s /i
-del nyqrelide\lib\CVS /q
-rmdir nyqrelide\lib\CVS /q
 xcopy demos nyqrelide\demos /s /i
-del nyqrelide\demos\jones\docs /q
-rmdir nyqrelide\demos\jones\docs /q
-del nyqrelide\demos\jones\nydoc /q
-rmdir nyqrelide\demos\jones\nydoc /q
-del nyqrelide\demos\jones\sjlib /q
-rmdir nyqrelide\demos\jones\sjlib /q
-del nyqrelide\demos\jones /q
-rmdir nyqrelide\demos\jones /q
-del nyqrelide\demos\plight\kit /q
-rmdir nyqrelide\demos\plight\kit /q
-del nyqrelide\demos\plight /q
-rmdir nyqrelide\demos\plight /q
-del nyqrelide\demos\CVS /q
-rmdir nyqrelide\demos\CVS /q
-del nyqrelide\demos\pmorales\CVS /q
-rmdir nyqrelide\demos\pmorales\CVS /q
-copy liblo\test-client\Release\osc-test-client.exe nyqrelide
-copy liblo\ser-to-osc\Release\ser-to-osc.exe nyqrelide
-copy NyqIDE\NyqIDE.exe nyqrelide
-copy NyqIDE\doc\Tips.htm nyqrelide\doc
-copy NyqIDE\doc\NyquistIDE.gif nyqrelide\doc
-copy NyqIDE\doc\nyqide_plot.gif nyqrelide\doc
+rem copy liblo\test-client\Release\osc-test-client.exe nyqrelide
+rem copy liblo\ser-to-osc\Release\ser-to-osc.exe nyqrelide
+rem copy NyqIDE\NyqIDE.exe nyqrelide
+rem copy NyqIDE\doc\Tips.htm nyqrelide\doc
+rem copy NyqIDE\doc\NyquistIDE.gif nyqrelide\doc
+rem copy NyqIDE\doc\nyqide_plot.gif nyqrelide\doc
 copy advantages.txt nyqrelide
-copy readme.txt nyqrelide
-copy todo.txt nyqrelide
+copy Readme.txt nyqrelide
+rem copy todo.txt nyqrelide
 copy license.txt nyqrelide
 mkdir nyqrelide\jnyqide
 copy Release\ny.exe nyqrelide\jnyqide\nyquist.exe
